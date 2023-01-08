e12f4af507060676fbf76f4f0fb59d7b5b67b32c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-01-08 20:36:06 +0900

    🚀 feat: WavePortalコントラクトで自分自身にwaveを送ってみる

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,7 +8,28 @@ import "hardhat/console.sol";
 // contract = 他の言語で言うところのclass
 // 1つのコンストラクタしか持つことができない
 contract WavePortal {
+
+    // 自動的に0に初期化される状態変数
+    // WavePotalコントラクトのストレージに永続的に保存される
+    // ※unit256は非常に大きな数を扱うことができる
+    uint256 totalWaves;
+
     constructor() {
         console.log("Here is my first smart contract!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log();
+
+        // msg.sender・・・関数を呼び出した人（=自分）にwaveを送った人のウォレットアドレスが入ってくる
+        // スマートコントラクトに含まれる関数を呼び出すには、ユーザは有効なウォレットを接続する必要がある
+        // msg.senderでは誰が関数を呼び出したかを正確に把握し、ユーザ認証を行なっている
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -1,6 +1,13 @@
 import { ethers, hardhatArguments } from 'hardhat';
+import { WavePortal } from '../typechain-types';
 
+// run.js
+// デバッグ用のテストコード
+// 本番環境でユーザが自分のスマートコントラクトを呼び出すシチュエーションを想定して、
+// コードが問題なく走るかテストする
 const main = async() => {
+    const [owner, randomPerson] = await ethers.getSigners();
+
     // getContractFactoryによってWavePotalコントラクトがコンパイルされる
     // コントラクトがコンパイルされると、コントラクトを扱うために必要なファイルがartifactsディレクトリ直下に生成される
     const waveContractFactory = await ethers.getContractFactory("WavePortal");
@@ -15,7 +22,20 @@ const main = async() => {
     // Hardhat（Solidityの実行環境）は実際に自分のマシン上にマイナーを作成してブロックチェーンを構築してくれる
     const wavePortal = await waveContract.deployed();
   
+    // 自分のスマートコントラクトのデプロイ先のアドレス（= wavePortal.address）をターミナルに出力している
     console.log("WavePortal address: ", wavePortal.address);
+    // WavePortalコントラクトをデプロイした人（= 自分）のアドレス（= owner.address）をターミナルに出力している
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount = await waveContract.getTotalWaves();
+  
+    // ブロックチェーン上の書き込みが発生するので、ガス代がかかる
+    let waveTxn = await waveContract.wave();
+
+    // 承認が終わったらトランザクションの結果を取得
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 }
 
 const runMain = async () => {
